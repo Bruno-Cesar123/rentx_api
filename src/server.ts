5ee38684dbba2b0561c49e13efd7650834c970fd
Bruno Cesar <brunocesar12386@gmail.com>
2021-10-11 16:44:28 -0300

    config debug

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 
-const app = express()
+const app = express();
+
+app.use(express.json());
 
 app.get("/", (request, response) => {
   return response.json({ message: "Hello World" })
