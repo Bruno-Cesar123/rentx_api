5a6605f2bd682504d671ad75e0e4bbbb2f7f2e8f
Bruno Cesar <brunocesar12386@gmail.com>
2021-10-25 14:44:48 -0300

    feat: create date provider

--- src/modules/rentals/useCases/createReantal/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createReantal/CreateRentalUseCase.ts
@@ -1,5 +1,6 @@
 import { Rental } from '@modules/rentals/infra/typeorm/entities/Rental';
 import { IRentalsRepository } from '@modules/rentals/repositories/IRentalsRepository';
+import { IDateProvider } from '@shared/container/providers/DateProvider/IDateProvider';
 import { AppError } from '@shared/errors/AppError';
 
 interface IRequest {
@@ -9,13 +10,18 @@ interface IRequest {
 }
 
 class CreateRentalUseCase {
-  constructor(private rentalsRepositoy: IRentalsRepository) {}
+  constructor(
+    private rentalsRepositoy: IRentalsRepository,
+    private dateProvider: IDateProvider,
+  ) {}
 
   async execute({
     user_id,
     car_id,
     expected_return_date,
   }: IRequest): Promise<Rental> {
+    const minimumHour = 24;
+
     const carUnavailable = await this.rentalsRepositoy.findOpenRentalByCar(
       car_id,
     );
@@ -30,6 +36,17 @@ class CreateRentalUseCase {
 
     if (rentalOpenToUser) {
       throw new AppError("There's a rental in progress for user!");
+    }
+
+    const dateNow = this.dateProvider.dateNow();
+
+    const compare = this.dateProvider.compareInHours(
+      dateNow,
+      expected_return_date,
+    );
+
+    if (compare < minimumHour) {
+      throw new AppError('Invalid return time');
     }
 
     const rental = this.rentalsRepositoy.create({
