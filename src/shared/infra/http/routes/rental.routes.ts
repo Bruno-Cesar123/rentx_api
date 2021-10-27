b20ee8cedc07308a562946e75e79708ce5427b99
Bruno Cesar <brunocesar12386@gmail.com>
2021-10-27 18:51:11 -0300

    test: refactor tests

--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 
-import { CreateRentalController } from '@modules/rentals/useCases/createReantal/CreateRentalController';
+import { CreateRentalController } from '@modules/rentals/useCases/createRental/CreateRentalController';
 import { DevolutionRentalController } from '@modules/rentals/useCases/devolutionRental/DevolutionRentalController';
 import { ListRentalsByUserController } from '@modules/rentals/useCases/listRentalsByUser/ListRentalsByUserController';
 
