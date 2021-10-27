3c82227471d692175cb8de986d50e183ba7bed0d
Bruno Cesar <brunocesar12386@gmail.com>
2021-10-27 10:59:40 -0300

    feat: create list rentals by user use case

---
+++ src/modules/rentals/useCases/listRentalsByUser/ListRentalsByUserController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { ListRentalsByUseCase } from './ListRentalsByUserUseCase';
+
+class ListRentalsByUserController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { id } = request.user;
+
+    const listRentalsByUseCase = container.resolve(ListRentalsByUseCase);
+
+    const rentals = await listRentalsByUseCase.execute(id);
+
+    return response.json(rentals);
+  }
+}
+
+export { ListRentalsByUserController };
