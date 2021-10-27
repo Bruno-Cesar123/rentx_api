eb5e84843771c9dcddf2fd0551a5ea9c511b0025
Bruno Cesar <brunocesar12386@gmail.com>
2021-10-27 20:39:52 -0300

    refactor: implements refresh token in authenticate

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: '3aee7ca68f7d6946a75c376eb188c84c',
+  expires_in_token: '15m',
+  secret_refresh_token: '12c9f31fd6cd2176b76b1984d23d4774',
+  expires_in_refresh_token: '30d',
+  expires_refresh_token_days: 30,
+};
