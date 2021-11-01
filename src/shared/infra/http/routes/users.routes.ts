0c7ecff51cfb282a620d8111b43a775af59b70ed
Bruno Cesar <brunocesar12386@gmail.com>
2021-11-01 15:19:06 -0300

    feat: create local storage provider avatar

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -3,13 +3,13 @@ import multer from 'multer';
 
 import uploadConfig from '@config/upload';
 import { CreateUserController } from '@modules/accounts/useCases/createUser/CreateUserController';
-import { UpdateUserAvatarController } from '@modules/accounts/useCases/UpdateUserAvatar/UpdateUserAvatarController';
+import { UpdateUserAvatarController } from '@modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController';
 
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 
 const usersRoutes = Router();
 
-const uploadAvatar = multer(uploadConfig.upload('./tmp/avatar'));
+const uploadAvatar = multer(uploadConfig);
 
 const createUserController = new CreateUserController();
 const updateUserAvatarController = new UpdateUserAvatarController();
