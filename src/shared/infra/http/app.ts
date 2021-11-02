b2f89125256d3481da2e49f12db18ccdacda4cd6
Bruno Cesar <brunocesar12386@gmail.com>
2021-11-02 20:40:31 -0300

    feat: add cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import 'dotenv/config';
+import cors from 'cors';
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
@@ -17,7 +18,7 @@ import { router } from './routes';
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
